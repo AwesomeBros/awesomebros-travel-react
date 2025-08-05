629a372ac988533dcaaa05da53fac4af6ffc9732
Gyulo94 <qkrcjfrb112@gmail.com>
2025-08-05 19:03:34 +0900

    init: 리액트 쿼리 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import QueryProvider from "./components/provider/query-provider.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <QueryProvider>
+    <App />
+  </QueryProvider>
+);
